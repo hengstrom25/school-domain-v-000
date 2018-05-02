9a24a43ad61908f39655c97ef4f839e85ce1e394
Heidi Engstrom <hengstrom25@gmail.com>
2018-05-02 16:42:11 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,15 +9,15 @@ class School
   def roster
     @roster
   end
-  
+
   def add_student(name, grade)
     if @roster[grade] != nil
-    @roster[grade] << name
+      @roster[grade] << name
     else
-    @roster[grade] = name
+      @roster[grade] = [name]
     end
   end
-  
+
   def grade(grade)
     @roster[grade]
   end
